41a9ea3d4efea7eaf69aa62a25fbb32a2a6df8a9
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-05-30 17:48:39 +0100

    Add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
+import { Routes, Route } from "react-router";
+import LoadingSpinner from './common/components/LoadingSpinner';
+
+const AppLayout = React.lazy(() => import("./layout/AppLayout"));
+const HomePage = React.lazy(() => import("./pages/HomePage/HomePage"));
+const SearchPage = React.lazy(() => import("./pages/SearchPage/SearchPage"));
+const SearchWithKeywordPage = React.lazy(() => import("./pages/SearchWithKeywordPage/SearchWithKeywordPage"));
+const PlaylistDetailPage = React.lazy(() => import("./pages/PlaylistDetailPage/PlaylistDetailPage"));
+
+//0. sidebar (playlist, menu)
+//1. landing page /
+//2. search page /search
+//3. serach results page /search/:keyword
+//4. playlist detail page /playlist/:id
+
+//5. (mobile) seperate playlist page /playlist
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<LoadingSpinner size={50} color="#ff6347" />}>
+      <Routes>
+        <Route path="/" element={<AppLayout/>}>
+          <Route index element = {<HomePage/>} />
+          <Route path="search" element={<SearchPage/>}/>
+          <Route path="search/:keyword" element={<SearchWithKeywordPage />}/>
+          <Route path="playlist/:id" element={<PlaylistDetailPage />}/>
+          {/* <Route path="playlist" element={<PlaylistPage/>}/> */}
+        </Route>
+      </Routes>
+    </Suspense> 
   );
 }
 
---
+++ src/common/components/LoadingSpinner.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './LoadingSpinner.css';
+
+interface SpinnerProps {
+  size?: number;
+  color?: string;
+}
+
+const Spinner: React.FC<SpinnerProps> = ({ size = 60, color = '#3498db' }) => {
+  const style: React.CSSProperties = {
+    width: size,
+    height: size,
+    border: `${size / 7.5}px solid #f3f3f3`,
+    borderTop: `${size / 7.5}px solid ${color}`,
+  };
+
+  return (
+    <div className="spinner-wrapper">
+        <div className="spinner" style={style} />
+    </div>
+  );
+};
+
+export default Spinner;
