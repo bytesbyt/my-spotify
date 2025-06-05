d14b882cf409520a5e2136b1b407cbf3d1d32d29
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-06-05 05:17:12 +0100

    Create album type

--- src/apis/albumApi.ts
+++ src/apis/albumApi.ts
@@ -1,10 +1,18 @@
 import axios from "axios";
+import { SPOTIFY_BASE_URL } from "../configs/commonConfig";
 
-export const getNewReleases = async() => {
+export const getNewReleases = async (clientCredentialToken:string) => {
     try {
-        const response = await axios.get(`address`)
-    }catch (error) {
+        const response = await axios.get(`${SPOTIFY_BASE_URL}/browse/new-releases`,
+        {
+            headers: {
+                Authorization: `Bearer ${clientCredentialToken}`,
+            },
+        }
 
-    };
-
-}
+        );
+        return response.data;
+    } catch (error) {
+        throw new Error("fail to fetch new release");
+    }
+};
---
+++ src/models/album.ts
@@ -0,0 +1,30 @@
+import { Artists } from "./artists";
+import { ExternalUrls, Images, Restrictions } from "./commonType";
+
+export interface getNewReleasesResponse {
+    album: {
+        href: string;
+        limit: number;
+        next: string;
+        offset: number;
+        previous: string | null;
+        total: number;
+        items: {
+            album_type: string;
+            total_tracks: number;
+            available_markets: string[];
+            external_urls: ExternalUrls;
+            href: string;
+            id: string;
+            images: Images[];
+            name: string;
+            release_date: string;
+            release_date_precision: string;
+            restrictions?: Restrictions;
+            type: string;
+            uri: string;
+            artists: Artists[];
+        }[];
+    };
+
+}
