5c74dc12f3b92031355233e4dc8e62b797ea0bb1
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-06-05 19:16:40 +0100

    Add play button

--- src/common/components/Card.tsx
+++ src/common/components/Card.tsx
@@ -1,50 +1,66 @@
-import { styled, Typography } from '@mui/material'
-import React from 'react'
-
-const CardContainer = styled("div")(({theme}) => ({
+import { styled, Typography } from "@mui/material";
+import React from "react";
+import PlayButton from "./PlayButton";
+const CardContainer = styled("div")(({ theme }) => ({
   minWidth: "160px",
   width: "100%",
   height: "100%",
   padding: "12px",
   borderRadius: "8px",
   "&:hover": {
-    backgroundCOlor: theme.palette.action.hover,
+    backgroundColor: theme.palette.action.hover,
     transform: "translate3d(0px, 0px, 0px)",
-    transition: "opacity 0.3x ease-in-out",
+    transition: "opacity 0.3s ease-in-out",
   },
   "&:hover .overlay": {
     opacity: 1,
   },
-
 }));
-
 const AlbumImage = styled("img")({
   width: "100%",
   height: "auto",
   borderRadius: "8px",
   marginBottom: "8px",
-})
+});
+const EllipsisTypography = styled(Typography)({
+  whiteSpace: "nowrap",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+});
+
+const Overlay = styled("div")({
+  position: "absolute",
+  bottom: "20px",
+  right: "8px",
+  opacity: 0,
+  transform: "translate3d(0px, 0px, 0px)",
+  transition: "opacity 0.3s ease-in-out",
+});
 
 interface CardProps {
-    name: string;
-    image: string;
-    artistName: string | undefined;
+  name: string | undefined;
+  artistName?: string | undefined;
+  image: string | undefined;
 }
 
-
-
-const Card = ({image, name, artistName}: CardProps) => {
+const Card = ({ name, artistName, image }: CardProps) => {
   return (
     <CardContainer>
-      <div style = {{ position: "relative" }}>
-        <AlbumImage src= {image}/>
+      <div style={{ position: "relative" }}>
+        <AlbumImage src={image} />
+        <Overlay className="overlay">
+          <PlayButton />
+        </Overlay>
       </div>
 
-      <Typography>{name}</Typography>
-      <Typography>{artistName}</Typography>
+      <EllipsisTypography variant="h2">{name || "No name"}</EllipsisTypography>
+      <EllipsisTypography variant="body1" color="text.secondary">
+        {artistName || "No artist"}
+      </EllipsisTypography>
     </CardContainer>
-
   );
 };
 
 export default Card;
+
+
