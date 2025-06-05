2f81b501fc8af10679bd1b56ce626987ebb18367
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-06-05 16:54:00 +0100

    Add card and update .env files

--- src/pages/HomePage/components/NewReleases.tsx
+++ src/pages/HomePage/components/NewReleases.tsx
@@ -1,18 +1,46 @@
 import React from 'react'
-import { Typography } from '@mui/material'
+import { Typography, Grid, styled} from '@mui/material';
 import useGetNewReleases from '../../../hooks/useGetNewReleases';
+import LoadingSpinner from '../../../common/components/LoadingSpinner';
+import ErrorMessage from '../../../common/components/ErrorMessage';
+import Card from '../../../common/components/Card';
 
 const NewReleases = () => {
-    const {data, error, isLoading} = useGetNewReleases();
-    console.log("ddd", data);
+  const {data, error, isLoading} = useGetNewReleases();
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+  if(error){
+    return <ErrorMessage errorMessage={error.message}/>
+  }
   return (
     <div>
       <Typography variant = "h1" paddingTop= "8px">
         New Released Albums
       </Typography>
+      {data && data.albums.items.length> 0 ? (
+        <Grid container spacing = {2}>
+          {data.albums.items.map((album) => (
+            <Grid size = {{xs:6, sm:4, md:2}} key = {album.id}>
+              <Card
+                image = {album.images[0].url}
+                name = {album.name}
+                artistName = {album.artists[0].name}
+              />
+            </Grid>
+
+          ))}
+
+        </Grid>
+
+
+
+      ) : (
+        <Typography variant = "h2">No Data</Typography>
+      )}
     </div>
   );
 };
 
 
-export default NewReleases
+export default NewReleases;
