8fc0071e67e2eb1bf66779fc7bfecbab7bdea959
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-06-07 02:18:23 +0100

    add profile image circle design

--- src/layout/components/Navbar.tsx
+++ src/layout/components/Navbar.tsx
@@ -1,11 +1,21 @@
-import { Box } from "@mui/material";
+import { Avatar, Box, styled } from "@mui/material";
 import React from 'react'
 import LoginButton from "../../common/components/LoginButton";
 import useGetCurrentUserProfile from "../../hooks/useGetCurrentUserProfile";
 
+const ProfileContainer = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  cursor: "pointer",
+  borderRadius: "8px",
+  padding: "0 8px",
+  height: "100%",
+});
 
 const Navbar = () => {
-  const {data: userProfile} = useGetCurrentUserProfile()
+  const {data: userProfile} = useGetCurrentUserProfile();
+  const profileImage = userProfile?.images?.[0]?.url || "";
+
   return (
     <Box 
         display="flex"
@@ -13,10 +23,16 @@ const Navbar = () => {
         alignItems="center"
         height= "64px"
     >
-      {userProfile ? <img src= {userProfile.images[0]?.url} /> : <LoginButton />}
-      
+      {userProfile ? (
+        <ProfileContainer>
+            <Avatar
+            src= {profileImage}
+            alt= {userProfile.display_name}
+            />        
+        </ProfileContainer>
+
+    ) : (<LoginButton />)}
     </Box>
   )
 }
-
 export default Navbar
