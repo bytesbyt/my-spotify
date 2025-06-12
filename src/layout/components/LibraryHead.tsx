54518cc15f62d7ca5c291cf6dacecdff2da72d78
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-06-12 17:46:59 +0100

    Add getspotifyauthurl function to handlecreateplaylist

--- src/layout/components/LibraryHead.tsx
+++ src/layout/components/LibraryHead.tsx
@@ -3,6 +3,9 @@ import { styled, Box, Typography, Button } from "@mui/material"
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import AddIcon from '@mui/icons-material/Add';
 import useCreatePlaylist from '../../hooks/useCreatePlaylist';
+import { useNavigate } from 'react-router';
+import useGetCurrentUserProfile from '../../hooks/useGetCurrentUserProfile';
+import { getSpotifyAuthUrl } from '../../utils/auth';
 
 const Head = styled("div")({
     display: "flex",
@@ -12,10 +15,21 @@ const Head = styled("div")({
 });
 
 const LibraryHead = () => {
+  const navigate = useNavigate();
+
+  const {data: user, isLoading: isUserLoading } = useGetCurrentUserProfile();
   const {mutate: createPlaylist} = useCreatePlaylist()
-    const handleCreatePlaylist = () => {
-      createPlaylist({ name: "My Playlist"});
-    };
+
+  const handleCreatePlaylist = () => {
+    if (!isUserLoading && !user) {
+      getSpotifyAuthUrl();
+      return;
+    }
+
+    if (user) {
+      createPlaylist({ name: "My Playlist", description: "A new playlist"});
+    }   
+  };
 
   return (
     <Head>
