8868023985ec03e8cea08bd408a47563511a0523
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-06-13 14:16:32 +0100

    Revert "Add logout drop down menu to the user profile"
    This reverts commit cb6cf6dc02d7bc1022ae6556778fc00952c8bd9b.

--- src/layout/components/Navbar.tsx
+++ src/layout/components/Navbar.tsx
@@ -1,38 +1,39 @@
-import { Box, styled } from '@mui/material';
-import React from 'react';
-import LoginButton from '../../common/components/LoginButton';
-import useGetCurrentUserProfile from '../../hooks/useGetCurrentUserProfile';
-import fallbackImage from './blank_profile.png';
-import ProfileDropdown from './ProfileDropdown';
-import { handleLogout } from '../../utils/authUtils';
-import { useNavigate } from 'react-router';
+import { Avatar, Box, styled } from "@mui/material";
+import React from 'react'
+import LoginButton from "../../common/components/LoginButton";
+import useGetCurrentUserProfile from "../../hooks/useGetCurrentUserProfile";
+import fallbackImage from "./blank_profile.png"
 
-const ProfileContainer = styled('div')({
-  display: 'flex',
-  alignItems: 'center',
-  cursor: 'pointer',
-  borderRadius: '8px',
-  padding: '0 8px',
-  height: '100%',
+const ProfileContainer = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  cursor: "pointer",
+  borderRadius: "8px",
+  padding: "0 8px",
+  height: "100%",
 });
 
 const Navbar = () => {
-  const { data: userProfile } = useGetCurrentUserProfile();
+  const {data: userProfile} = useGetCurrentUserProfile();
   const profileImage = userProfile?.images?.[0]?.url || fallbackImage;
 
   return (
-    <Box
-      display="flex"
-      justifyContent="flex-end"
-      alignItems="center"
-      height="64px"
+    <Box 
+        display="flex"
+        justifyContent="flex-end"
+        alignItems="center"
+        height= "64px"
     >
       {userProfile ? (
-        <ProfileDropdown userProfile={userProfile} onLogout={handleLogout} />
-      ) : (
-        <LoginButton />
-      )}
+        <ProfileContainer>
+            <Avatar
+            src= {profileImage}
+            alt= {userProfile.display_name}
+            />        
+        </ProfileContainer>
+
+    ) : (<LoginButton />)}
     </Box>
-  );
-};
-export default Navbar;
+  )
+}
+export default Navbar
--- src/models/profile.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "./user";
-
-export interface ProfileDropdownProps {
-  userProfile: User;
-  onLogout: () => void;
-}
